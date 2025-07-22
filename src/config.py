9a918ddd442c5c8962a460caa70a8551ad8da8b3
kkind <84040776@qq.com>
2025-07-22 13:42:55 +0800

    更新配置文件，修改 AI_URL 为新的地址，并注释掉旧的 URL。

--- src/config.py
+++ src/config.py
@@ -23,10 +23,11 @@ class Config:
     DOC_PATH = os.environ.get("DOC_PATH", os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "docs"))
 
     # vLLM 配置
-    AI_URL = os.environ.get("AI_URL", "http://ds.kaoxve.com:9999/v1")
+    #AI_URL = os.environ.get("AI_URL", "http://ds.kaoxve.com:9999/v1")
+    AI_URL = os.environ.get("AI_URL", "http://192.168.91.210:8000/v1")
     AI_MODEL = os.environ.get("AI_MODEL", "Qwen3-32B")
     AI_KEY = os.environ.get("AI_KEY", "bori-qwen3-2012")
-
+   
     # 其它自定义配置可在此添加
 
 config = Config()
